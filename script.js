d091a13cc7961f3446b172efe3989c76f85ea0bb
Vitor Alves <alvesvi314@gmail.com>
2026-08-07 19:03:43 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const downloadButton = document.querySelector(".download-button");
-
-if (downloadButton) {
-  downloadButton.addEventListener("click", (event) => {
-    if (downloadButton.getAttribute("href") === "#") {
-      event.preventDefault();
-    }
-  });
-}
