283a7eddb266aea204fc3d5e362710d5893a68de
Vitor Alves <alvesvi314@gmail.com>
2026-08-08 10:31:05 -0400

    Delete Site directory

--- Site/script.js
+++
@@ -1,9 +0,0 @@
-const downloadButtons = document.querySelectorAll(".download-button");
-
-downloadButtons.forEach((downloadButton) => {
-  downloadButton.addEventListener("click", (event) => {
-    if (downloadButton.getAttribute("href") === "#") {
-      event.preventDefault();
-    }
-  });
-});
